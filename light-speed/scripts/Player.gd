946423ec3541ce36c22240fa9b1e1a4c1a9a965a
jacknick7 <josep.ciurana@est.fib.upc.edu>
2021-06-19 12:56:36 +0200

    added level scene

--- light-speed/scripts/Player.gd
+++ light-speed/scripts/Player.gd
@@ -13,19 +13,19 @@ func _ready():
 # TODO: adjust the multipliers to delta for best inherce feeling & control
 # TODO: add space Input makes ship jump
 func _process(delta):
-	if Input.is_action_pressed("ui_right"):
-		$AnimatedSprite.animation = "turn_left"
-		velocity = min(velocity+(delta*0.25),max_velocity)
-	elif Input.is_action_pressed("ui_left"):
-		$AnimatedSprite.animation = "turn_right"
-		velocity = max(velocity-(delta*0.25),-max_velocity)
-	elif Input.is_action_just_pressed("ui_select"):
+	if Input.is_action_just_pressed("ui_select"):
 		var dis_rotation = rotation - (PI / 2)
 		var new_direction = Vector2(cos(dis_rotation),sin(dis_rotation)) 
 		# Normalize new_direction vector?
 		var new_position = position + (new_direction * 192)
 		# Here new_position must be converted to the center of the nearest hexagon
 		position = new_position
+	elif Input.is_action_pressed("ui_right"):
+		$AnimatedSprite.animation = "turn_left"
+		velocity = min(velocity+(delta*0.25),max_velocity)
+	elif Input.is_action_pressed("ui_left"):
+		$AnimatedSprite.animation = "turn_right"
+		velocity = max(velocity-(delta*0.25),-max_velocity)
 	else:
 		$AnimatedSprite.animation = "idle"
 		if velocity > 0:
@@ -39,4 +39,4 @@ func _process(delta):
 			rotation -= 2 * PI 
 		elif rotation < 0:
 			rotation += 2 * PI
-	print(str(rotation))
+	#print(str(rotation))
