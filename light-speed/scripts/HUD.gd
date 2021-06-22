3819813576129603accf7b5fd97ef41d3856851e
jacknick7 <josep.ciurana@est.fib.upc.edu>
2021-06-22 23:38:55 +0200

    expanded GUI

---
+++ light-speed/scripts/HUD.gd
@@ -0,0 +1,45 @@
+extends CanvasLayer
+
+
+signal start_game(easy)
+
+
+func _ready():
+	$ScoreLabel.hide()
+	$MessageLabel.hide()
+
+
+func show_message(text):
+	$MessageLabel.text = text
+	$MessageLabel.show()
+	$MessageTimer.start()
+
+
+func show_game_over():
+	show_message("Game Over")
+	yield($MessageTimer, "timeout")
+	$ScoreLabel.hide()
+	$TitleLabel.show()
+	yield(get_tree().create_timer(1), "timeout")
+	$VBoxContainer.show()
+
+
+func update_score(score):
+	$ScoreLabel.text = str(score)
+
+
+func _on_ExitButton_pressed():
+	get_tree().quit()
+
+
+func _on_StartButton_pressed():
+	$VBoxContainer.hide()
+	$TitleLabel.hide()
+	$ScoreLabel.show()
+	emit_signal("start_game", true)	
+	yield(get_tree().create_timer(1), "timeout")
+	show_game_over()
+
+
+func _on_MessageTimer_timeout():
+	$MessageLabel.hide()
