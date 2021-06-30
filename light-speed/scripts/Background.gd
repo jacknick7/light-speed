b7df905e7df96047c3aec323257f7cda6331af7e
jacknick7 <josep.ciurana@est.fib.upc.edu>
2021-07-01 01:10:02 +0200

    added background alpha v0.1

--- light-speed/scripts/Background.gd
+++ light-speed/scripts/Background.gd
@@ -1,13 +1,9 @@
-extends Node
+extends TextureRect
 
 
 func _ready():
 	var screen_size = get_viewport().size
-	var scale_x = screen_size.x / 512
-	var scale_y = screen_size.y / 512
-	$BackgroundColor.set_scale(Vector2(scale_x,scale_y))
-	$BackgroundColor.set_position(Vector2(screen_size.x/2,screen_size.y/2))
 	randomize()
 	var id = randi() % 6
-	$BackgroundColor.texture = load("res://textures/background/background_"+str(id)+".jpg")
+	texture = load("res://textures/background/"+str(screen_size.y)+"/background_"+str(id)+".jpg")
 	print(str(id))
