8a58780aa6fdddb89d88a30f1ba85db6af398776
jacknick7 <josep.ciurana@est.fib.upc.edu>
2021-08-07 20:05:45 +0200

    leaderboard frontend completed

--- light-speed/scripts/OverMenu.gd
+++ light-speed/scripts/OverMenu.gd
@@ -1,14 +1,32 @@
 extends MarginContainer
 
 
+onready var name_labels = [
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer2/NameLabel1,
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer2/NameLabel2,
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer2/NameLabel3,
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer2/NameLabel4,
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer2/NameLabel5,
+]
+onready var score_labels = [
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer3/ScoreLabel1,
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer3/ScoreLabel2,
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer3/ScoreLabel3,
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer3/ScoreLabel4,
+	$VBoxContainer/CenterContainer2/HBoxContainer/VBoxContainer3/ScoreLabel5,
+]
+
 func update_score(score):
 	$VBoxContainer/CenterContainer/VBoxContainer/ScoreLabel.text = str(score) + " points"
+	var names
+	var scores
 	var file = File.new()
 	var err = file.open("user://leaderboard.save", File.READ)
 	if err == OK:
 		var leaderboard = parse_json(file.get_line())
-		var names = leaderboard["names"]
-		var scores = leaderboard["scores"]
+		file.close()
+		names = leaderboard["names"]
+		scores = leaderboard["scores"]
 		var i = 0
 		var found = 0
 		while i < scores.size() && found == 0:
@@ -27,8 +45,7 @@ func update_score(score):
 					j -= 1
 			scores[i] = score
 			var daytime = OS.get_datetime()
-			names[i] = str(daytime["day"]) + '.' + str(daytime["month"]) + '.' + str(daytime["year"])
-			file.close()
+			names[i] = str(daytime["day"]) + '.' + str(daytime["month"]) + '.' + str(daytime["year"]) + '.' +str(daytime["hour"]) + '.' + str(daytime["minute"]) + '.' + str(daytime["second"])
 			err = file.open("user://leaderboard.save", File.WRITE)
 			if err == OK:
 				file.store_line(to_json({"names":names, "scores":scores}))
@@ -37,3 +54,12 @@ func update_score(score):
 		file.close()
 	else:
 		print("Error " + err + " opening leaderboard for reading")
+	var ind = 0
+	while ind < scores.size():
+		if scores[ind] == null:
+			name_labels[ind].text = "..."
+			score_labels[ind].text = "..."
+		else:
+			name_labels[ind].text = names[ind]
+			score_labels[ind].text = str(scores[ind])
+		ind += 1
