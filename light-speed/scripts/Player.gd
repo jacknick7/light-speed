07fa3f30be5c8cd73ab321af36048e24afce96c1
jacknick7 <josep.ciurana@est.fib.upc.edu>
2021-08-21 21:13:11 +0200

    added 2 jumping distances + minor fixes

--- light-speed/scripts/Player.gd
+++ light-speed/scripts/Player.gd
@@ -4,6 +4,7 @@ extends Node
 var playing
 var easy
 var velocity
+var short_jump
 const MAX_VELOCITY = 0.125
 const ACC_VELOCITY = 0.25
 const DACC_VELOCITY = 0.25
@@ -28,34 +29,31 @@ func _process(delta):
 
 
 func handle_input(delta):
-	if Input.is_action_just_pressed("ui_select"):
+	if Input.is_action_just_pressed("ui_up"):
+		short_jump = !short_jump
+		if easy: update_position_easy()
+	elif Input.is_action_just_pressed("ui_select"):
 		var next_position = new_position()
 		if is_inside_screen($PlayerShip.position + next_position):
 			$PlayerShip.position += next_position
 			if easy: update_position_easy()
 		else: emit_signal("screen_limits")
 	elif Input.is_action_pressed("ui_right"):
-		$PlayerShip/AnimatedSprite.animation = "turn_left"
+		if $PlayerShip/AnimatedSprite.animation != "turn_left": $PlayerShip/AnimatedSprite.animation = "turn_left"
 		velocity = min(velocity + (delta * ACC_VELOCITY), MAX_VELOCITY)
 	elif Input.is_action_pressed("ui_left"):
-		$PlayerShip/AnimatedSprite.animation = "turn_right"
+		if $PlayerShip/AnimatedSprite.animation != "turn_right": $PlayerShip/AnimatedSprite.animation = "turn_right"
 		velocity = max(velocity - (delta * ACC_VELOCITY), -MAX_VELOCITY)
 	else:
-		$PlayerShip/AnimatedSprite.animation = "idle"
-		if velocity > 0:
-			velocity = max(velocity - (delta * DACC_VELOCITY), 0)
-		elif velocity < 0:
-			velocity = min(velocity + (delta * DACC_VELOCITY), 0)
+		if $PlayerShip/AnimatedSprite.animation != "idle": $PlayerShip/AnimatedSprite.animation = "idle"
+		if velocity > 0: velocity = max(velocity - (delta * DACC_VELOCITY), 0)
+		elif velocity < 0: velocity = min(velocity + (delta * DACC_VELOCITY), 0)
 	if velocity != 0:
 		$PlayerShip.rotation += velocity
 		# Make rotation between [0,2*PI]
-		if $PlayerShip.rotation > 2 * PI:
-			$PlayerShip.rotation -= 2 * PI 
-		elif $PlayerShip.rotation < 0:
-			$PlayerShip.rotation += 2 * PI
-		if easy:
-			update_position_easy()
-	#print(str(rotation))
+		if $PlayerShip.rotation > 2 * PI: $PlayerShip.rotation -= 2 * PI 
+		elif $PlayerShip.rotation < 0: $PlayerShip.rotation += 2 * PI
+		if easy: update_position_easy()
 
 
 func update_position_easy():
@@ -74,18 +72,21 @@ func is_inside_screen(pos):
 func new_position():
 	var new_pos
 	var ps_rot = $PlayerShip.rotation
-	if ps_rot >= ROT_POINTS[0] && ps_rot < ROT_POINTS[1]: new_pos = Vector2(128,-192)
-	elif ps_rot >= ROT_POINTS[1] && ps_rot < ROT_POINTS[2]: new_pos = Vector2(192,-96)
-	elif ps_rot >= ROT_POINTS[2] && ps_rot < ROT_POINTS[3]: new_pos = Vector2(256,0)
-	elif ps_rot >= ROT_POINTS[3] && ps_rot < ROT_POINTS[4]: new_pos = Vector2(192,96)
-	elif ps_rot >= ROT_POINTS[4] && ps_rot < ROT_POINTS[5]: new_pos = Vector2(128,192)
-	elif ps_rot >= ROT_POINTS[5] && ps_rot < ROT_POINTS[6]: new_pos = Vector2(0,192)
-	elif ps_rot >= ROT_POINTS[6] && ps_rot < ROT_POINTS[7]: new_pos = Vector2(-128,192)
-	elif ps_rot >= ROT_POINTS[7] && ps_rot < ROT_POINTS[8]: new_pos = Vector2(-192,96)
-	elif ps_rot >= ROT_POINTS[8] && ps_rot < ROT_POINTS[9]: new_pos = Vector2(-256,0)
-	elif ps_rot >= ROT_POINTS[9] && ps_rot < ROT_POINTS[10]: new_pos = Vector2(-192,-96)
-	elif ps_rot >= ROT_POINTS[10] && ps_rot < ROT_POINTS[11]: new_pos = Vector2(-128,-192)
-	else:  new_pos = Vector2(0,-192) # $PlayerShip.rotation in [(23 * PI) / 12, PI / 12)
+	if short_jump:
+		pass
+	else:
+		if ps_rot >= ROT_POINTS[0] && ps_rot < ROT_POINTS[1]: new_pos = Vector2(128,-192)
+		elif ps_rot >= ROT_POINTS[1] && ps_rot < ROT_POINTS[2]: new_pos = Vector2(192,-96)
+		elif ps_rot >= ROT_POINTS[2] && ps_rot < ROT_POINTS[3]: new_pos = Vector2(256,0)
+		elif ps_rot >= ROT_POINTS[3] && ps_rot < ROT_POINTS[4]: new_pos = Vector2(192,96)
+		elif ps_rot >= ROT_POINTS[4] && ps_rot < ROT_POINTS[5]: new_pos = Vector2(128,192)
+		elif ps_rot >= ROT_POINTS[5] && ps_rot < ROT_POINTS[6]: new_pos = Vector2(0,192)
+		elif ps_rot >= ROT_POINTS[6] && ps_rot < ROT_POINTS[7]: new_pos = Vector2(-128,192)
+		elif ps_rot >= ROT_POINTS[7] && ps_rot < ROT_POINTS[8]: new_pos = Vector2(-192,96)
+		elif ps_rot >= ROT_POINTS[8] && ps_rot < ROT_POINTS[9]: new_pos = Vector2(-256,0)
+		elif ps_rot >= ROT_POINTS[9] && ps_rot < ROT_POINTS[10]: new_pos = Vector2(-192,-96)
+		elif ps_rot >= ROT_POINTS[10] && ps_rot < ROT_POINTS[11]: new_pos = Vector2(-128,-192)
+		else:  new_pos = Vector2(0,-192) # $PlayerShip.rotation in [(23 * PI) / 12, PI / 12)
 	return new_pos
 
 
@@ -93,6 +94,7 @@ func start_game(diff, pos):
 	easy = diff
 	playing = true
 	velocity = 0
+	short_jump = false
 	$PlayerShip.position = pos
 	$PlayerShip.rotation = 0	
 	$PlayerShip/AnimatedSprite.animation = "idle"
